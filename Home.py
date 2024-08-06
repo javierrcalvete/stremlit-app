999f225d11553868977e5c9ac2bf942d8477c4f1
javi <javierrcalvete@gmail.com>
2024-08-06 11:26:50 -0300

    testeing charts

--- Home.py
+++ Home.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.figure_factory as ff
 
+st.sidebar.header('Retails app')
 
 st.title("hello world")
 
@@ -9,7 +11,7 @@ st.header('This is a header with divider', divider='rainbow')
 
 st.markdown("this is with markdown")
 
-col1, col2 = st.columns(2)
+
 
 
 
@@ -19,6 +21,8 @@ st.write('value of :red[x] is', x)
 
 st.subheader('area chart')
 
+col1, col2 = st.columns(2)
+
 chart_data = pd.DataFrame(
     {
         "col1": np.random.randn(20),
@@ -27,4 +31,37 @@ chart_data = pd.DataFrame(
     }
 )
 
-st.area_chart(chart_data, x="col1", y="col2", color="col3")
+with col1:
+
+    st.dataframe(chart_data,)
+
+with col2:
+    
+    st.area_chart(chart_data, x="col1", y="col2", color="col3")
+
+st.subheader('math')
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+
+st.divider()
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
